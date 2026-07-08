36d40d13a5b0659b0d9fdf1441afc421763e14f1
brandon68 <67684399+brandon68@users.noreply.github.com>
2026-07-08 13:31:03 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,6 @@ from telegram.ext import (
 from datetime import datetime
 import nest_asyncio
 import os
-from datetime import datetime, time
 
 nest_asyncio.apply()
 
@@ -124,41 +123,7 @@ def generar_factura():
 
     return f"Fact_{str(nuevo).zfill(3)}"
 
-# =========================
-# AVISOS AUTOMÁTICOS
-# =========================
 
-async def revisar_vencimientos(context: ContextTypes.DEFAULT_TYPE):
-
-    df = leer_datos()
-
-    if df.empty:
-        return
-
-    hoy = pd.Timestamp.now().normalize()
-
-    for _, row in df.iterrows():
-
-        if pd.isna(row["Fecha_Vencimiento"]):
-            continue
-
-        fecha = pd.Timestamp(row["Fecha_Vencimiento"]).normalize()
-
-        if fecha == hoy:
-
-            mensaje = (
-                "🚨 *HOY VENCE UNA CUENTA*\n\n"
-                f"👤 Cliente: *{row['Cliente']}*\n"
-                f"📺 Servicio: *{row['Servicio']}*\n"
-                f"📅 Vence: *{fecha.strftime('%d/%m/%Y')}*\n"
-                f"🔑 Cuenta:\n`{row['Cuenta']}`"
-            )
-
-            await context.bot.send_message(
-                chat_id=CHAT_ID,
-                text=mensaje,
-                parse_mode="Markdown"
-            )
 # =========================
 # GUARDAR CSV
 # =========================
@@ -605,11 +570,4 @@ app.add_handler(CommandHandler("modificar", modificar))
 app.add_handler(CommandHandler("id", id))
 
 print("✅ Bot corriendo...")
-
-job_queue = app.job_queue
-
-job_queue.run_daily(
-    revisar_vencimientos,
-    time=time(hour=9, minute=0)
-)
 app.run_polling()
